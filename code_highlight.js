aaa1ab8d3c1fdaca77a929addaacd47413e453fc
Brian Leathem <bleathem@gmail.com>
2012-06-15 13:38:05 -0700

    Refactored the code and added some docs

--- code_highlight.js
+++ code_highlight.js
@@ -12,54 +12,22 @@ var CodeHighlight = (function ($) {
         }, options);
 
         var Reveal = settings.reveal;
-        Reveal.addEventListener('fragmentshown', show_lines);
+        Reveal.addEventListener('fragmentshown', showLines);
         Reveal.addEventListener('fragmenthidden', hide_lines);
     }
 
-    function show_lines(event) {
+    function showLines(event) {
         $(event.fragment).each(function (count, elem) {
             var code = $(this);
             highlight_fragment(code);
         })
     }
 
-    function highlight_fragment(fragment) {
-        var code = get_code(fragment);
-        if (typeof code !== 'undefined') {
-            var new_html = "";
-
-            var text_lines = code.orig.code.html().split('\n');
-            var between = false;
-            var padding = 0;
-            for (var i = 0; i < text_lines.length; i++) {
-                var line = text_lines[i]
-                if (i == code.from) {
-                    between = true;
-                    padding = determine_left_padding(line);
-
-                    var new_line = line.substring(padding);
-                    new_html += '<' + 'pre' + ' class="prettyprint highlight">\n'
-                    new_html += ('<' + 'code' + ' class="prettyprint">' + new_line + '\n')
-                }
-                else {
-                    new_html += (between ? line.substring(padding) : line) + '\n'
-                }
-                if (i == code.to) {
-                    between = false;
-                    new_html += '</code></pre>'
-                }
-            }
-
-            code.block.html(new_html);
-            prettyPrint();
-            $('.present .highlight').trigger('lineshighlighted');
-        }
-    }
-
     function hide_lines(event) {
         $(event.fragment).each(function (i, elem) {
             var fragment = $(this);
-            var code = get_code(fragment);
+            var coords = get_coords(fragment);
+            var code = get_code(coords);
             if (typeof code !== 'undefined') {
                 code.block.html(code.orig.code.html());
                 prettyPrint();
@@ -70,24 +38,101 @@ var CodeHighlight = (function ($) {
         })
     }
 
-    function get_code(fragment) {
+    function highlight_fragment(fragment) {
+        var coords = get_coords(fragment);
+        if (typeof coords !== 'undefined') {
+            highlight_code(coords);
+        }
+    }
+
+    /**
+     * Highlights the code specified by the coords object
+     * @param coords
+     */
+    function highlight_code(coords) {
+        var code = get_code(coords);
+        var new_html = "";
+
+        var text_lines = code.orig.code.html().split('\n');
+        var between = false;
+        var padding = 0;
+        for (var i = 0; i < text_lines.length; i++) {
+            var line = text_lines[i]
+            if (i == coords.from) {
+                between = true;
+                padding = determine_left_padding(line);
+
+                var new_line = line.substring(padding);
+                new_html += '<' + 'pre' + ' class="prettyprint highlight">\n'
+                new_html += ('<' + 'code' + ' class="prettyprint">' + new_line + '\n')
+            }
+            else {
+                new_html += (between ? line.substring(padding) : line) + '\n'
+            }
+            if (i == coords.to) {
+                between = false;
+                new_html += '</code></pre>'
+            }
+        }
+
+        code.block.html(new_html);
+        prettyPrint();
+        $('.present .highlight').trigger('lineshighlighted');
+    }
+
+    /**
+     * Creates a coords object from the data-* attributes of the given fragment parameter.
+     *
+     * fragment refers to a DOM object that must have 2 data attributes defined:
+     *  -       data-prettify: the id of the <code> element to be highlighted
+     *  - data-prettify_lines: the lines to highlight in "to:from" string syntax
+     *
+     * coords is an object with three properties:
+     *  -   id: the id of the <code> element to be highlighted
+     *  - from: the first line to highlight
+     *  -   to: the last line to highlight
+     *
+     * @param fragment
+     * @return coords
+     */
+    function get_coords(fragment) {
         if (typeof fragment.attr('data-prettify') === 'undefined') {
             return undefined;
         }
+        var coords = {};
+        coords.id = fragment.attr('data-prettify');
+        var lines = fragment.attr('data-prettify_lines').split(':');
+        coords.from = lines[0];
+        coords.to = lines[1];
+        return coords;
+    }
+
+    /**
+     * Create a code object from the given coords parameter.
+     *
+     * The code object has the following properties:
+     * -             id: the id of the <code> element to be highlighted
+     * -     code.block: the jQuery object of the <code> element to be highlighted
+     * -   code.orig.id: the id of the un-displayed element that contains the original markup of the <code> element
+     * - code.orig.code: the original markup of the <code> element
+     * @param coords
+     * @return code
+     */
+    function get_code(coords) {
+        if (typeof coords === 'undefined') {
+            return undefined;
+        }
         var code = {};
-        var prettify = fragment.attr('data-prettify')
-        code.lines = fragment.attr('data-prettify_lines').split(':');
-        code.from = code.lines[0];
-        code.to = code.lines[1];
-        code.block = $('#' + prettify);
+        code.id = coords.id;
+        code.block = $('#' + coords.id);
         code.orig = {};
-        code.orig.id = 'code_' + prettify;
+        code.orig.id = 'code_' + coords.id;
         code.orig.code = $('#' + code.orig.id);
         if (code.orig.code.length == 0) {
             $('<code />', {
-                id:code.orig.id,
-                style:"display:none",
-                html:code.block.html()
+                id: code.orig.id,
+                style: "display:none",
+                html: code.block.html()
             }).appendTo('body');
             code.orig.code = $('#' + code.orig.id);
         }
@@ -104,7 +149,7 @@ var CodeHighlight = (function ($) {
     }
 
     return {
-        initialize:initialize
+        initialize: initialize
     }
 
 })(jQuery);
